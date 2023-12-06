17f6c83a226fb40941da7abc77b85a4f250d7047
Julius Ojakaa <ojayallday@gmail.com>
2023-12-07 02:49:51 +0300

    constants class set up

---
+++ src/main/java/ke/co/safaricom/constants/constants.java
@@ -0,0 +1,16 @@
+package ke.co.safaricom.constants;
+
+public class constants {
+
+    public static final String newBorn = "NEWBORN";
+    public static final String young = "YOUNG";
+    public static final String adult ="ADULT";
+    public static final String healthy = "HEALTHY";
+    public static final String ill= "ILL";
+    public static final String okay = "OKAY";
+    public static final String normal= "NORMAL";
+    public static final String endangered = "ENDANGERED";
+    public static final String zoneA = "ZONEA";
+    public static final String nearRiver= "NEAR_RIVER";
+    public static final String neQuadrant = "NE_QUADRANT";
+}
